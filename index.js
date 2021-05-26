a89ed5437ed512c99c23af69422ec5a7e56dad50
SERGIO ESTRADA VALENZUELA <sergioev@mx1.ibm.com>
2021-05-26 14:27:10 -0500

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ hi: "There" });
+	res.send({ bye: "Body" });
 });
 
 // using port from an env variable
