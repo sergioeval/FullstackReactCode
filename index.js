219e07f739b324048ceae9b2dc29a2995e61c500
SERGIO ESTRADA VALENZUELA <sergioev@mx1.ibm.com>
2021-05-26 17:02:03 -0500

    updates

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send({ bye: "Body" });
-});
+passport.use(new GoogleStrategy());
 
 // using port from an env variable
 const PORT = process.env.PORT || 5000;
