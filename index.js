6dac74ea8011bbe0c50407fded50b2a7d4094813
SERGIO ESTRADA VALENZUELA <sergioev@mx1.ibm.com>
2021-07-02 12:26:05 -0500

    updates

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+// this is needed to express works correctly when it is in the production env
+// this will run only in production
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets
+  app.use(express.static("client/build"));
+  // express will serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // using port from an env variable
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
